34793c40efe9cc43a74c9e43c25cb5f285a91f5b
Cobraking364 <gustav.emil.k@gmail.com>
2026-01-05 13:23:14 +0100

    File structure

---
+++ src/Board.java
@@ -0,0 +1,18 @@
+package src;
+public class Board {
+    int sizeX;
+    int sizeY;
+
+    Board(int x, int y){
+        sizeX = x;
+        sizeY = y;
+    }
+
+    public int getSizeX(){
+        return sizeX;
+    }
+
+    public int getSizeY(){
+        return sizeY;
+    }
+}
