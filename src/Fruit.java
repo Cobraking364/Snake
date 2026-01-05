b214239a5c2f318b8445a71dc813fd40ca1bb47f
Cobraking364 <gustav.emil.k@gmail.com>
2026-01-05 15:44:12 +0100

    Fruit tied in correctly

--- src/Fruit.java
+++ src/Fruit.java
@@ -8,27 +8,29 @@ public class Fruit {
     public void respawn(int sizeX, int sizeY, List<Position> snakeBody) {
         List<Position> possiblePositions = new ArrayList<>();
         for (int i = 0; i < sizeX; i++) {
-            for (int j = 0; j < sizeY; j++){
-                possiblePositions.add(new Position(i , j));
+            for (int j = 0; j < sizeY; j++) {
+                possiblePositions.add(new Position(i, j));
             }
         }
         int listIndex = 0;
         Collections.shuffle(possiblePositions);
-        while(isInList(possiblePositions.get(listIndex), snakeBody)) {
+        while (isInList(possiblePositions.get(listIndex), snakeBody)) {
             listIndex++;
         }
-        position =  possiblePositions.get(listIndex);
-                
-        }
+        position = possiblePositions.get(listIndex);
 
-        private boolean isInList(Position position, List<Position> list) {
-            for (Position position2 : list) {
-                if (position.equals(position2)) {
-                    return true;
-                }
-            }
-            return false;
-        }
     }
 
+    private boolean isInList(Position position, List<Position> list) {
+        for (Position position2 : list) {
+            if (position.equals(position2)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public Position getPosition(){
+        return position;
+    }
+}
