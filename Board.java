a061d2db6233c0e0346b5ba18c510e8a5fc67203
Cobraking364 <gustav.emil.k@gmail.com>
2026-01-05 13:14:07 +0100

    Idk

---
+++ Board.java
@@ -0,0 +1,17 @@
+public class Board {
+    int sizeX;
+    int sizeY;
+
+    Board(int x, int y){
+        sizeX = x;
+        sizeY = y;
+    }
+
+    public int getSizeX(){
+        return sizeX;
+    }
+
+    public int getSizeY(){
+        return sizeY;
+    }
+}
