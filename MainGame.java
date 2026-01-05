309d3966cb001971e603c26a1a30161fd805f9ac
Cobraking364 <gustav.emil.k@gmail.com>
2026-01-05 11:55:54 +0100

    Base

---
+++ MainGame.java
@@ -0,0 +1,5 @@
+class MainGame{
+    public static void main(String[] args){
+
+    }
+}
---
+++ Snake.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class Snake {
+    ArrayList<Position> bends = new ArrayList<Position>();
+    Position headPos = new Position(0, 0);
+    int len;
+}
