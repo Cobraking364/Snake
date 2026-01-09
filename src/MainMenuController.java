87d3e64479ac8314d39eeb4b2dab8636e357df8b
Mikkel-png <s256208@dtu.dk>
2026-01-09 13:55:30 +0100

    add settings button MainMenuController.java

--- src/MainMenuController.java
+++ src/MainMenuController.java
@@ -9,7 +9,7 @@ public class MainMenuController {
     private MainMenuView view;
     private Scene scene;
 
-    public MainMenuController(MainMenuView view, Scene scene, int gameWidth, int gameHeight, SceneManager sceneManager) {
+    public MainMenuController(MainMenuView view, Scene scene, int gameWidth, int gameHeight, Settings settings, SceneManager sceneManager) {
         this.view = view;
         this.scene = scene;
 
@@ -28,6 +28,17 @@ public class MainMenuController {
             @Override
             public void handle(ActionEvent event) {
                 Platform.exit();
+            }
+
+        });
+
+        view.getSettingsButton().setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                SettingsView settingsView = new SettingsView((int)view.getWidth(),(int)view.getHeight(),settings);
+                Scene settingsScene = new Scene(settingsView);
+                sceneManager.changeScene(settingsScene);
+                SettingsController settingsController = new SettingsController(settingsView, settingsScene, gameWidth, gameHeight, settings, sceneManager);
             }
 
         });
