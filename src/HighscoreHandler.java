b4d6b58edcd1a18036afd7af731e6bbd371b294e
Skye-Arrow <119897680+Skye-Arrow@users.noreply.github.com>
2026-01-09 16:31:05 +0100

    Update HighscoreHandler.java

--- src/HighscoreHandler.java
+++ src/HighscoreHandler.java
@@ -7,63 +7,76 @@ import java.util.*;
 //Reads file to hashmap with bufferedreader
 //When score is achieved compare to key in hashmap
 //If score > value, wipe .txt and write hashmap to file
-
 public class HighscoreHandler {
-    public void highscoreHandler(Board boardSize, int score) {
-         
-        String board = boardSize + "";
-        Map<String,String> map = writeFileToMap(board, score);
-        compareBoardToKey(map, board, score);
+
+    public static void main(String[] args) {
+
+        checkHighScore(10, 10, 40);
+    }
+
+    static void checkHighScore(int x, int y, int score) {
+
+        String boardSize = x + ":" + y;
+        Map<String, String> map = loadMapFromFile();
+
+        String highScore = map.getOrDefault(boardSize, "0");
+
+        if (Integer.parseInt(highScore) > score) {
+            map.put(boardSize, score + "");
+        }
+
         writeMapToFile(map);
 
     }
 
-    public Map<String,String> writeFileToMap(String board, int score) {
+//InputStream highscoreFile = HighscoreHandler.class.getResourceAsStream("/highscore.txt");
+    public static Map<String, String> loadMapFromFile() {
         Map<String, String> highscoreMap = new HashMap<>();
-        try (InputStream highscoreFile = HighscoreHandler.class.getResourceAsStream("/highscore.txt");
-                BufferedReader writeToMap = new BufferedReader(new InputStreamReader(highscoreFile))) {
+        try (InputStream highscoreFile = HighscoreHandler.class.getResourceAsStream(getFileName()); BufferedReader reader = new BufferedReader(new InputStreamReader(highscoreFile))) {
+            if (highscoreFile == null) {
+                throw new RuntimeException("highscore.txt not found on classpath");
+            }
             String line;
-            while ((line = writeToMap.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(" ");
                 String key = parts[0];
                 String value = parts[1];
                 highscoreMap.put(key, value);
             }
         } catch (IOException e) {
-            System.out.println("couldn't read file");
+            System.out.println("couldn't read file1");
         }
         return highscoreMap;
     }
-    public Map<String,String> compareBoardToKey(Map<String,String> map, String board, int score){
-        boolean exists = false;
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            if (board.equals(entry.getKey())){
-                exists = true;
-            }
+
+    public static Map<String, String> updateScoreMap(Map<String, String> map, String boardSize, int score) {
+
+        if (!map.containsKey(boardSize)) {
+            map.put(boardSize, "0");
         }
-        if (!exists) {
-            map.put(board, "0");
-        }
-        if (Integer. parseInt(map.get(board)) < score) {
-            map.put(board, score+"");
+        if (Integer.parseInt(map.get(boardSize)) < score) {
+            map.put(boardSize, score + "");
         }
         return map;
     }
-    public void writeMapToFile(Map<String, String> map) {
-        BufferedWriter writeToFile = null;
-       try (FileOutputStream name = new FileOutputStream("/highscore.txt", false)) { //cleans file
-       } catch (IOException e) {
-            System.out.println("couldn't read file");
-        }
 
+    public static void writeMapToFile(Map<String, String> map) {
 
-        try { writeToFile = new BufferedWriter(new FileWriter("/highscore.txt"));
+        try (FileWriter writer = new FileWriter(HighscoreHandler.class.getResource(getFileName()).toString())) {
+            String str = "";
             for (Map.Entry<String, String> entry : map.entrySet()) {
-                writeToFile.write(entry.getKey() + " " + entry.getValue());
-                writeToFile.newLine();
+                str += (entry.getKey() + " " + entry.getValue() + "\n");
             }
+            writer.write(str);
+            System.out.println("the string is: " + str);
         } catch (IOException e) {
-            System.out.println("couldn't read file");
+
+            System.out.println(e.getMessage() + "couldn't read file3");
         }
     }
+
+    public static String getFileName() {
+        return "/resources/highscore.txt";
+    }
+
 }
