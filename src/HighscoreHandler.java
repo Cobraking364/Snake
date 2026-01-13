b1eb0481461febdc9c085cab3d872229f50dd29b
Skye-Arrow <119897680+Skye-Arrow@users.noreply.github.com>
2026-01-13 15:02:54 +0100

    Update HighscoreHandler.java

--- src/HighscoreHandler.java
+++ src/HighscoreHandler.java
@@ -62,7 +62,7 @@ public class HighscoreHandler {
 
     public static void writeMapToFile(Map<String, String> map) {
 
-        try (FileWriter writer = new FileWriter(HighscoreHandler.class.getResource(getFileName()).toString())) {
+        try (FileWriter writer = new FileWriter(HighscoreHandler.class.getResource(getFileName())) {
             String str = "";
             for (Map.Entry<String, String> entry : map.entrySet()) {
                 str += (entry.getKey() + " " + entry.getValue() + "\n");
