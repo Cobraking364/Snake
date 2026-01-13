1dd114796eaf0ff8f59e51020968f43a9aad66ba
Skye-Arrow <Rumlekatten3@gmail.com>
2026-01-13 16:37:27 +0100

    Finish HighscoreHandler
    also added highscore.txt to /Snake

--- src/HighscoreHandler.java
+++ src/HighscoreHandler.java
@@ -3,39 +3,25 @@ package src;
 import java.io.*;
 import java.util.*;
 
-//Create new hashmap
-//Reads file to hashmap with bufferedreader
-//When score is achieved compare to key in hashmap
-//If score > value, wipe .txt and write hashmap to file
 public class HighscoreHandler {
-
-    public static void main(String[] args) {
-
-        checkHighScore(10, 10, 40);
-    }
-
     static void checkHighScore(int x, int y, int score) {
 
         String boardSize = x + ":" + y;
         Map<String, String> map = loadMapFromFile();
 
-        String highScore = map.getOrDefault(boardSize, "0");
-
-        if (Integer.parseInt(highScore) > score) {
+        String oldHighscore = map.getOrDefault(boardSize, "0");
+        if (Integer.parseInt(oldHighscore) < score) {
             map.put(boardSize, score + "");
         }
 
         writeMapToFile(map);
-
     }
 
-    // InputStream highscoreFile =
-    // HighscoreHandler.class.getResourceAsStream("/highscore.txt");
     public static Map<String, String> loadMapFromFile() {
         Map<String, String> highscoreMap = new HashMap<>();
-        File highscoreFile = new File (getFileName());
-        if (!highscoreFile.exists()){
-            System.out.println("can't find highscore.txt");
+        File highscoreFile = new File(getFileName());
+        if (!highscoreFile.exists()) {
+            System.out.println("can't find " + getFileName());
         }
         try (BufferedReader reader = new BufferedReader(new FileReader(getFileName()))) {
             String line;
@@ -46,34 +32,20 @@ public class HighscoreHandler {
                 highscoreMap.put(key, value);
             }
         } catch (IOException e) {
-            System.out.println("couldn't read file1");
+            System.out.println("can't find " + getFileName());
         }
         return highscoreMap;
     }
 
-    public static Map<String, String> updateScoreMap(Map<String, String> map, String boardSize, int score) {
-
-        if (!map.containsKey(boardSize)) {
-            map.put(boardSize, "0");
-        }
-        if (Integer.parseInt(map.get(boardSize)) < score) {
-            map.put(boardSize, score + "");
-        }
-        return map;
-    }
-
     public static void writeMapToFile(Map<String, String> map) {
-
         try (PrintWriter writer = new PrintWriter(getFileName())) {
-            String str = "";
+            String mapData = "";
             for (Map.Entry<String, String> entry : map.entrySet()) {
-                str += (entry.getKey() + " " + entry.getValue() + "\n");
+                mapData += (entry.getKey() + " " + entry.getValue() + "\n");
             }
-            writer.write(str);
-            System.out.println("the string is: " + str);
+            writer.write(mapData);
         } catch (IOException e) {
-
-            System.out.println(e.getMessage() + "couldn't read file3");
+            System.out.println(e.getMessage() + "can't find " + getFileName());
         }
     }
 
